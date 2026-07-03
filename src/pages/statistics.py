9780cfab0ba900966d4d4baa48d5fdc89e9ee7f3
Sandeep-060 <tharalasandeep2005@gmail.com>
2026-07-03 12:56:10 +0530

    statistical analysis

--- src/pages/statistics.py
+++ src/pages/statistics.py
@@ -1,5 +1,105 @@
 import streamlit as st
+from src.services.statistics_analyzer import (
+    calculate_statistics,
+    calculate_categorical_statistics,
+)
 
 def show_statistics_page():
+    """
+    Displays numerical statistics
+    for uploaded datasets.
+    """
+
     st.title("📊 Statistics")
-    st.info("Statistics module will be implemented later.")
+
+    # Check dataset
+    if st.session_state.dataframe is None:
+        st.warning("⚠ Please upload a dataset first.")
+        return
+
+    df = st.session_state.dataframe
+
+    st.success("Dataset loaded successfully!")
+
+    st.divider()
+
+    st.subheader("Select Numerical Column")
+
+    numeric_columns = df.select_dtypes(include="number").columns.tolist()
+
+    if len(numeric_columns) == 0:
+        st.error("No numerical columns found.")
+        return
+
+    selected_column = st.selectbox(
+        "Choose a column",
+        numeric_columns
+    )
+
+    st.divider()
+
+    statistics = calculate_statistics(df[selected_column])
+
+    st.subheader(f"📈 Statistics for '{selected_column}'")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.metric("Mean", f"{statistics['Mean']:.2f}")
+        st.metric("Variance", f"{statistics['Variance']:.2f}")
+        st.metric("Minimum", statistics["Minimum"])
+        st.metric("Q1", f"{statistics['Q1']:.2f}")
+
+    with col2:
+        st.metric("Median", f"{statistics['Median']:.2f}")
+        st.metric(
+            "Std Deviation",
+            f"{statistics['Standard Deviation']:.2f}"
+        )
+        st.metric("Maximum", statistics["Maximum"])
+        st.metric("Q3", f"{statistics['Q3']:.2f}")
+
+ 
+    with col3:
+        st.metric("Mode", statistics["Mode"])
+        st.metric("IQR", f"{statistics['IQR']:.2f}")
+        st.metric("Skewness", f"{statistics['Skewness']:.2f}")
+        st.metric("Kurtosis", f"{statistics['Kurtosis']:.2f}")
+
+    st.divider()
+
+    st.subheader("🏷 Categorical Statistics")
+
+    categorical_columns = df.select_dtypes(exclude="number").columns.tolist()
+    if len(categorical_columns) == 0:
+        st.info("No categorical columns found.")
+        return
+    selected_category = st.selectbox(
+            "Choose a categorical column",
+            categorical_columns
+        )
+    category_statistics = calculate_categorical_statistics(
+                        df[selected_category]
+                        )
+    
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.metric(
+            "Unique Values",
+            category_statistics["Unique Values"]
+        )
+
+    with col2:
+        st.metric(
+            "Most Common",
+            category_statistics["Most Common"]
+        )
+    st.subheader("Frequency Table")
+
+    st.dataframe(
+        category_statistics["Frequency Table"],
+        width="stretch",
+        hide_index=True
+    )
+
---
+++ src/services/statistics_analyzer.py
@@ -0,0 +1,71 @@
+import pandas as pd
+
+
+def calculate_statistics(series: pd.Series):
+    """
+    Calculates descriptive statistics
+    for a numerical column.
+
+    Parameters
+    ----------
+    series : pandas.Series
+
+    Returns
+    -------
+    dict
+    """
+
+    # Remove missing values
+    series = series.dropna()
+
+    mode_values = series.mode()
+
+    if len(mode_values) == 0:
+        mode = "No Mode"
+    else:
+        mode = ", ".join(map(str, mode_values.tolist()))
+        if len(mode_values) == 1:
+            mode = str(mode_values.iloc[0])
+
+        elif len(mode_values) <= 3:
+            mode = ", ".join(map(str, mode_values.tolist()))
+
+        else:
+            mode = f"{mode_values.iloc[0]}, {mode_values.iloc[1]} + {len(mode_values)-2} more"
+
+    statistics = {
+        "Mean": series.mean(),
+        "Median": series.median(),
+        "Mode": mode,
+        "Variance": series.var(),
+        "Standard Deviation": series.std(),
+        "Minimum": series.min(),
+        "Maximum": series.max(),
+        "Q1": series.quantile(0.25),
+        "Q3": series.quantile(0.75),
+        "IQR": series.quantile(0.75) - series.quantile(0.25),
+        "Skewness": series.skew(),
+        "Kurtosis": series.kurt()
+    }
+
+    return statistics
+
+def calculate_categorical_statistics(series: pd.Series):
+    """
+    Calculates statistics for a categorical column.
+    """
+
+    series = series.dropna()
+
+    frequencies = series.value_counts()
+
+    return {
+        "Unique Values": series.nunique(),
+        "Most Common": frequencies.index[0],
+        "Frequency Table": frequencies.reset_index().rename(
+            columns={
+                "index": "Category",
+                series.name: "Count"
+            }
+        )
+    }
